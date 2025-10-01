dad6d6d0a713b14948143ae8bf3e04bd01523ed6
JoseSamueldeMeloSantos <josesamuelsantos321@hotmail.com>
2025-09-30 22:49:51 -0300

    teste de integração para findbyname

--- src/test/java/br/com/bthirtyeight/integrationstest/controllers/withjson/PersonControllerJsonTest.java
+++ src/test/java/br/com/bthirtyeight/integrationstest/controllers/withjson/PersonControllerJsonTest.java
@@ -255,6 +255,52 @@ class PersonControllerJsonTest extends AbstractIntegrationTest {
 
     }
 
+    @Test
+    @Order(7)
+    void findByNameTest() throws JsonProcessingException {
+
+
+        var content =
+                given(specification)
+                        .accept(MediaType.APPLICATION_JSON_VALUE)
+                        .pathParam("firstName","and")
+                        .queryParam("page", 0,"size",12,"direction","asc")
+                        .when()
+                        .get("findPeopleByName/{firstName}")
+                        .then()
+                        .statusCode(200)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)//mediaType de retorno sera json
+                        .extract()
+                        .body()
+                        .asString();
+
+        WrapperPersonDTO wrapper = objectMapper.readValue(content,WrapperPersonDTO.class);
+        List<PersonDTO> people = wrapper.getEmbeddedDTO().getPeople();
+
+        //fazemos isso para nao ter problema com o restassure(converter obj -> str -> obj
+        PersonDTO personOne = people.get(0);
+
+        assertNotNull(personOne.getId());
+        assertTrue(personOne.getId() > 0);
+
+        assertEquals("Alessandro", personOne.getFirstName());
+        assertEquals("McFaul", personOne.getLastName());
+        assertEquals("5 Lukken Plaza", personOne.getAddress());
+        assertEquals("Male", personOne.getGender());
+        assertTrue(personOne.getEnabled());
+
+        PersonDTO personFour = people.get(4);
+
+        assertNotNull(personFour.getId());
+        assertTrue(personFour.getId() > 0);
+
+        assertEquals("Brandyn", personFour.getFirstName());
+        assertEquals("Grasha", personFour.getLastName());
+        assertEquals("96 Mosinee Parkway", personFour.getAddress());
+        assertEquals("Male", personFour.getGender());
+        assertTrue(personFour.getEnabled());
+    }
+
 
 
     private void mockPerson() {
