d6f0503c4fc43f36b669cc2e9c90a62c24c7298a
JoseSamueldeMeloSantos <josesamuelsantos321@hotmail.com>
2025-11-08 16:50:38 -0300

    AuthService

---
+++ src/main/java/br/com/bthirtyeight/services/AuthService.java
@@ -0,0 +1,48 @@
+package br.com.bthirtyeight.services;
+
+import br.com.bthirtyeight.data.dto.security.AccountCredentialsDTO;
+import br.com.bthirtyeight.data.dto.security.TokenDTO;
+import br.com.bthirtyeight.repository.UserRepository;
+import br.com.bthirtyeight.security.jwt.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+
+    @Autowired
+    private UserRepository repository;
+
+
+    public ResponseEntity<TokenDTO> signIN(AccountCredentialsDTO credentials) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        credentials.getUsername(),
+                        credentials.getPassword()
+                )
+        );
+
+        var user = repository.findByUsername(credentials.getUsername());
+
+        if (user == null) {
+            throw new UsernameNotFoundException("Username " + credentials.getUsername() + "not found!");
+        }
+
+        var tokenReponse = tokenProvider.createAcessToken(
+                credentials.getUsername(),
+                user.getRoles()
+        );
+
+        return ResponseEntity.ok(tokenReponse);
+    }
+}
